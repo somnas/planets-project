3a244318de5fac94033b985e9cf339c3fac6bdcc
Erik Somnäs <erik.somnas@gmail.com>
2022-09-09 17:35:25 +0200

    wip

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED';
+}
 
 fs.createReadStream('kepler_data.csv')
     .pipe(parse({
@@ -9,12 +13,14 @@ fs.createReadStream('kepler_data.csv')
         columns: true,
     }))
     .on('data', (data) => {
-        results.push(data);
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
     })
     .on('error', (err) => {
         console.log(err);
     })
     .on('end', () => {
-        console.log(results);
+        console.log(habitablePlanets);
         console.log('done');
     });
