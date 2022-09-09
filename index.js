6062e21fa9d2e709f2ee8b54c554227267e2bece
Erik Somnäs <erik.somnas@gmail.com>
2022-09-09 17:30:01 +0200

    add parse fx

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const results = [];
 
 fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
     .on('data', (data) => {
         results.push(data);
     })
@@ -14,5 +18,3 @@ fs.createReadStream('kepler_data.csv')
         console.log(results);
         console.log('done');
     });
-
-//parse();
